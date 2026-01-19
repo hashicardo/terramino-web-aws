12d30d413b307489ae94f1f1d92c77736d590981
ricardorompar <ricardo.romero@hashicorp.com>
2026-01-19 17:37:56 +0100

    changed etag definition to actual file content. Otherwise constant diffs

--- main.tf
+++ main.tf
@@ -71,17 +71,20 @@ resource "aws_s3_bucket_policy" "website" {
   depends_on = [aws_s3_bucket_public_access_block.website]
 }
 
+locals {
+  index_html = templatefile("${path.module}/web/index.html", {
+    debug_message = var.debug_message
+    region_name   = data.aws_region.current.region
+  })
+}
+
 # Upload index.html (with template processing)
 resource "aws_s3_object" "index_html" {
   bucket       = aws_s3_bucket.website.id
   key          = "index.html"
-  content      = templatefile("${path.module}/web/index.html", {
-                    debug_message = var.debug_message, 
-                    region_name = data.aws_region.current.region 
-                })
-
+  content      = local.index_html
   content_type = "text/html"
-  etag         = filemd5("${path.module}/web/index.html")
+  etag         = md5(local.index_html)
 }
 
 # Upload CSS file
